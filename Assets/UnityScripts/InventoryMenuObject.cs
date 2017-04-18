9ca4c673461a3d1614bb71fe6221d128408b1a1a
redflamegard <Corey.King@iam.local>
2017-04-18 14:23:09 -0500

    LLLEEEERRROOOOOYYYYY   JEEEENNNKKKKKIINS

---
+++ Assets/UnityScripts/InventoryMenuObject.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class InventoryMenuObject : MonoBehaviour
+{
+    //private GameManager GM;
+
+    public Toggle toggle;
+    private InventoryManager inventoryManager;
+
+    public InventoryObj InventoryObjectRepresented { get; set; }
+
+
+
+    public void ClickedThisMenuItem()
+    {
+        AudioSource audio = inventoryManager.GetComponent<AudioSource>();
+        //audio.Play();
+    }
+
+    public void ClickedDropItem()
+    {
+
+
+
+        InventoryObjectRepresented.DoDrop();
+
+        inventoryManager.ShowInventoryMenu();
+
+
+
+    }
+    public void ClickedEat()
+    {
+
+        InventoryObjectRepresented.DoEat();
+
+        inventoryManager.ShowInventoryMenu();
+
+
+
+
+    }
+
+
+    public void OnValueChanged()
+    {
+        if (toggle.isOn)
+        {
+
+            // update the description text based on the selected item
+            inventoryManager.UpdateDescriptionText(InventoryObjectRepresented.DescriptionText);
+        }
+
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+
+
+
+        try
+        {
+            inventoryManager = GameObject.Find("Inventory Manager").GetComponent<InventoryManager>();
+        }
+        catch
+        {
+            throw new System.Exception("Scene requires game object" +
+                " named \"Inventory Manager\" with an InventoryManager script attached");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
