5aadab9e43ff7fe8fa3b34d71f10ec6582fe9dd9
Mike Morrone <Hanegwen725@gmail.com>
2017-04-27 13:09:36 -0500

    Working on Menu System
    Instructions arn't done

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
