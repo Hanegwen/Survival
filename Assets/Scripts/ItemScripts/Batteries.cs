cc52b4c0117d68b59f1a02ad4963fd9a1b1416c5
redflamegard <Corey.King@iam.local>
2017-05-03 14:40:09 -0500

    UI and InventoryUpdate

--- Assets/Scripts/ItemScripts/Batteries.cs
+++ Assets/Scripts/ItemScripts/Batteries.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class Batteries : InventoryObj
 {
+    Flashlight flashLight;
     void Awake()
     {
+       flashLight = FindObjectOfType<Flashlight>();
         itemType = ItemType.ELECTRIC;
+    }
+
+    public override void UseItemOn(InventoryObj itemToUseOn)
+    {
+
+        if (itemToUseOn is Flashlight)
+        {
+            Debug.Log("Used batteries");
+            flashLight.RechargeFlashlight();
+            RemoveSelfFromInventory();
+            
+        }
     }
 }
